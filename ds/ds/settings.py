12c47b6d5e0a118da827ba7954dc874462b46b90
AleksandrTan <rumych@list.ru>
2017-10-26 11:49:46 +0300

    first

--- ds/ds/settings.py
+++ ds/ds/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dsdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
