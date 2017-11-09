c231bef7d9eac20080a2c2205f8567cec825e784
AleksandrTan <rumych@list.ru>
2017-11-09 10:08:53 +0200

    first

--- ds/ds/wsgi.py
+++ ds/ds/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('e:/WebProjects/ds.com/ds/')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ds.settings")
 
