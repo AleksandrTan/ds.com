85918959af58d49a2096405e8a25cfdc7999fedb
AleksandrTan <rumych@list.ru>
2017-12-11 14:37:24 +0200

    Products Work

--- ds/dsadminvn/templatetags/customfilters.py
+++ ds/dsadminvn/templatetags/customfilters.py
@@ -3,6 +3,9 @@ from django import template
 register = template.Library()
 
 def strint(value, arg):
-    return value[arg]
+    try:
+        return value[arg]
+    except IndexError:
+        return None
 
 register.filter('strint', strint)
