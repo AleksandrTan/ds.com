4daaf18bb7eb229a98144c05cd09f7d7081fe207
AleksandrTan <rumych@list.ru>
2018-08-23 18:25:26 +0300

    Barcode sales

--- ds/barcodesalep/static/js/barcodesalep.js
+++ ds/barcodesalep/static/js/barcodesalep.js
@@ -58,20 +58,23 @@ function FoundBarcode(e){
         var count_tr = this.hasChildElements();
         var new_product = '<tr id="add_ptr-'+count_tr+'"><td class="text-center"><input type="hidden" ' +
             'id="id_form-'+count_tr+'-products" name="form-'+count_tr+'-products" value="'+product_data.id+'"/>' +
-            '<input type="hidden" name="start_price" id="start_price" value="'+product_data.price+'"/>' +
-            '<input type="hidden" name="price" id="price" value="'+product_data.price+'"/>'+product_data.articul+'</td>' +
+            '<input type="hidden" name="start_price" id="start_price'+count_tr+'" value="'+product_data.price+'"/>' +
+            '<input type="hidden" name="price" id="price'+count_tr+'" value="'+product_data.price+'"/>'+product_data.articul+'</td>' +
             '<td class="text-center">'+product_data.category+'</td><td class="text-center">'+product_data.name+'</td>' +
-            '<td class="text-center" id="price_table">'+product_data.price+'</td><td class="text-center">'+product_data.discount+'%</td>' +
-            '<td class="text-center">'+product_data.price_discount+'</td>' +
+            '<td class="text-center" id="price_table'+count_tr+'">'+product_data.price+'</td><td class="text-center">'+product_data.discount+'%</td>' +
+            '<td class="text-center" id="price_discount-'+count_tr+'">'+product_data.price_discount+'</td>' +
             '<td><input id="id_form-'+count_tr+'-lost_num" name="form-'+count_tr+'-lost_num" type="number"></td><td class="text-center">'+product_data.size+'</td>' +
-            '<td class="text-center" id="in_stock">'+product_data.count_num+'</td><td class="text-center">1</td><td>' +
+            '<td class="text-center" id="in_stock'+count_tr+'">'+product_data.count_num+'</td><td class="text-center">1</td><td>' +
             '<textarea name="description"  rows="5"></textarea></td><td>' +
             '<button type="button" class="btn btn-sm btn-danger form-control" data-delete-product="delete-product" ' +
             'data-count-id="'+count_tr+'">Отмена</button></td></tr>';
         this.parenttable.append(new_product);
+        //change count forms
         this.totalForms.val(parseInt(this.totalForms.val()) + 1);
         this.barcode_articul.val('').focus();
-        this.total_amount_all.text(parseInt(this.total_amount_all.text()) + product_data.price_discount);
+        //calculate total sum
+        this.calculationOfAmount(product_data.price_discount);
+        //show button submit and total sum
         if (this.hasChildElements() == 1){
             this.buttonCancellCeil.show();
         }
@@ -82,8 +85,9 @@ function FoundBarcode(e){
         return this.parenttable.find('tr').length;
     };
     
-    this.calculationOfAmount = function () {
-        
+    this.calculationOfAmount = function (price_discount) {
+        this.total_amount_all.text(parseInt(this.total_amount_all.text()) + price_discount);
+        return true;
     }
 }
 
@@ -91,13 +95,6 @@ function FoundBarcode(e){
 $('#products_list').on('click', '[data-delete-product=delete-product]', function (e) {
     var deleteProduct = new DeleteProduct(e, $(this));
     deleteProduct.init();
-
-    // var identificator = $(this).attr('data-count-id');
-    // $('#add_ptr-'+identificator).remove();
-    // $('#id_form-TOTAL_FORMS').val(parseInt($('#id_form-TOTAL_FORMS').val()) - 1);
-    // if ($('#products_list').find('tr').length == 0){
-    //         $('#cancel_ceil').hide();
-    //     }
 });
 
 function DeleteProduct(e, obj){
@@ -123,11 +120,21 @@ function DeleteProduct(e, obj){
 
     this.deleteProduct = function () {
         var identificator = this.obj.attr('data-count-id');
+         this.calculationOfAmount(identificator);
         $('#add_ptr-'+identificator).remove();
-        $('#id_form-TOTAL_FORMS').val(parseInt($('#id_form-TOTAL_FORMS').val()) - 1);
-        if ($('#products_list').find('tr').length == 0){
-                $('#cancel_ceil').hide();
+        this.totalForms.val(parseInt(this.totalForms.val()) - 1);
+        if (this.hasChildElements() == 0){
+                this.buttonCancellCeil.hide();
             }
         this.preloader.css({'display':'none', 'opacity': '0.5'});
+    };
+
+    this.hasChildElements = function() {
+        return this.parenttable.find('tr').length;
+    };
+
+    this.calculationOfAmount = function (identificator) {
+        this.total_amount_all.text(parseInt(this.total_amount_all.text()) - parseInt($('#price_discount-'+identificator).text()));
+        return true;
     }
 }
