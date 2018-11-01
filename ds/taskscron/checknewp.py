fa2c368f9e3fa47262ce68698528608e9e1a2335
AleksandrTan <rumych@list.ru>
2018-11-01 10:33:25 +0200

    Peewee crontasks

--- ds/taskscron/checknewp.py
+++ ds/taskscron/checknewp.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-from taskscron.models import Modelss
+from taskscron.models import Modelss, Products
 
 
 def check_new():
     try:
-        Modelss.update(is_new_date_end=datetime.now()).where(Modelss.is_new_date_end <= datetime.now()).execute()
+        Modelss.update(is_new_date_end=datetime.now(), is_new=False).\
+            where(Modelss.is_new_date_end <= datetime.now()).execute()
+
+        Products.update(is_new_date_end=datetime.now(), is_new=False).\
+            where(Products.is_new_date_end <= datetime.now()).execute()
     except EOFError:
         pass
 
-check_new()
+if __name__ == '__main__':
+    check_new()
--- ds/taskscron/models.py
+++ ds/taskscron/models.py
@@ -30,3 +30,33 @@ class Modelss(Model):
     identifier = CharField(max_length=20)
     dirname_img = CharField(max_length=25, default='')
     date_create = DateTimeField()
+
+
+class Products(Model):
+    class Meta:
+        database = dbhandle
+        db_table = "dsstore_products"
+
+    id = PrimaryKeyField(null=False)
+    name = CharField(default='', max_length=50)
+    brends = SmallIntegerField(default=0)
+    season_id = SmallIntegerField(default=0)
+    price = FloatField(default=0.0)
+    wholesale_price =FloatField(default=0)
+    purshase_price =FloatField(default=0)
+    description = TextField(default='')
+    caption = CharField(default='', max_length=200)
+    color = CharField(max_length=100, default='')
+    discount = SmallIntegerField(default=0)
+    sale = BooleanField(default=False)
+    sale_price = FloatField(default=0)
+    sale_date_end = DateTimeField()
+    seo_attributes = TextField(default='')
+    is_belarus = BooleanField(default=False)
+    is_active = BooleanField(default=False)
+    is_new = BooleanField(default=False)
+    is_new_date_end = DateField()
+    link_name = CharField(max_length=550)
+    identifier = CharField(max_length=20)
+    dirname_img = CharField(max_length=25, default='')
+    date_create = DateTimeField()
