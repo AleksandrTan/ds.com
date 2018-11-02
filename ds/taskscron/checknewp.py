a87217afac9bafb60ac2a5334fe56256be0330ca
AleksandrTan <rumych@list.ru>
2018-11-02 14:14:40 +0200

    Peewee crontasks

--- ds/taskscron/checknewp.py
+++ ds/taskscron/checknewp.py
@@ -2,15 +2,27 @@ from datetime import datetime
 from taskscron.models import Modelss, Products
 
 
-def check_new():
-    try:
-        Modelss.update(is_new_date_end=datetime.now(), is_new=False).\
-            where(Modelss.is_new_date_end <= datetime.now()).execute()
+class CheckNewFeature:
 
-        Products.update(is_new_date_end=datetime.now(), is_new=False).\
-            where(Products.is_new_date_end <= datetime.now()).execute()
-    except EOFError:
-        pass
+    def check_new(self):
+        self.check_new_modelss()
+        self.check_new_products()
+
+    def check_new_modelss(self):
+        try:
+            Modelss.update(is_new_date_end=datetime.now(), is_new=False).\
+                where(Modelss.is_new_date_end <= datetime.now()).execute()
+        except EOFError:
+            pass
+
+    def check_new_products(self):
+        try:
+            Products.update(is_new_date_end=datetime.now(), is_new=False).\
+                where(Products.is_new_date_end <= datetime.now()).execute()
+        except EOFError:
+            pass
 
 if __name__ == '__main__':
-    check_new()
+
+    data_check = CheckNewFeature()
+    data_check.check_new()
